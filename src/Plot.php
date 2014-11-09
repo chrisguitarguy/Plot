9210ea930f448f1d2b223c0aec6925f0eeba5f49
Christopher Davis <cdavis9999@gmail.com>
2014-11-08 20:52:27 -0500

    A little bit of convenience

---
+++ src/Plot.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Plot
+ *
+ * @package     Chrisguitarguy\Plot
+ * @copyright   2014 Christopher Davis
+ * @license     http://opensource.org/licenses/mit MIT
+ */
+
+namespace Chrisguitarguy\Plot;
+
+/**
+ * A facade around the whole system
+ */
+class Plot
+{
+    private $parser;
+
+    public function __construct(Parser $parser=null)
+    {
+        $this->parser = $parser ?: new Parser();
+    }
+
+    public function evaluateString($str)
+    {
+        return $this->parser->parse(Input::fromString($str))->evaluate();
+    }
+
+    public function evaluateFile($filename)
+    {
+        return $this->parser->parser(Input::fromFiel($filename))->evaluate();
+    }
+}
